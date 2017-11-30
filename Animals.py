a6f2ac265634a91a51abe0fb0c195ebb1c8e4045
Oleg Arbuzov <boozze@mail.ru>
2017-11-30 23:24:51 +0300

    fixed

--- Animals.py
+++ Animals.py
@@ -1,142 +1,133 @@
 class Animal:
 
-    NAME = ""
-    COLOR = ""
-    IS_ALIVE = True
-    SPEED = 0
+    name = ""
+    color = ""
+    is_alive = True
+    speed = 0
 
     def __init__(self, name, color, is_alive, speed):
-        self.NAME = name
-        self.COLOR = color
-        self.IS_AlIVE = is_alive
-        self.SPEED = speed
+        self.name = name
+        self.color = color
+        self.is_alive = is_alive
+        self.speed = speed
 
     def eat(self):
-        print("{} is eating..".format(self.NAME))
+        print("{} is eating..".format(self.name))
 
     def move(self, moving_type):
         if moving_type == "walking":
-            print("{} is walking".format(self.NAME))
+            print("{} is walking".format(self.name))
         elif moving_type == "running":
-            print("{} is running".format(self.NAME))
+            print("{} is running".format(self.name))
 
     def stop(self):
-        self.SPEED = 0
+        self.speed = 0
 
     def is_moving(self):
-        if self.SPEED == 0:
+        if self.speed == 0:
             return True
         else:
             return False
 
     def die(self):
-        self.IS_ALIVE = False
+        self.is_alive = False
 
 
 class Cow(Animal):
-    def __init__(self, name, color, speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
+    def __init__(self, name, color, is_alive, speed):
+        super().__init__(name, color, is_alive, speed)
 
     def  moo(self):
         print("mooo")
 
 
 class Goat(Animal):
-    def __init__(self, name, color, speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
+    def __init__(self, name, color, is_alive, speed):
+        super().__init__(name, color, is_alive, speed)
 
     def bleat(self):
         print("meee")
 
 
 class Sheep(Animal):
-    def __init__(self, name, color, speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
+    def __init__(self, name, color, is_alive, speed):
+        super().__init__(name, color, is_alive, speed)
 
     def bleat(self):
         print("beee")
 
 
 class Sheep(Animal):
-    def __init__(self, name, color, speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
+    def __init__(self, name, color, is_alive, speed):
+        super().__init__(name, color, is_alive, speed)
 
     def grunt(self):
         print("naf naf uiii")
 
 
 class Bird(Animal):
-    FLYING_SPEED = 0
+    flying_speed = 0
 
-    def __init__(self, name, color, speed, flying_speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
-        self.FLYING_SPEED = flying_speed
+    def __init__(self, flying_speed, name, color, is_alive, speed):
+        super().__init__(name, color, is_alive, speed)
+        self.flying_speed = flying_speed
 
     def move(self, moving_type):
         if moving_type == "walking":
-            print("{} is walking".format(self.NAME))
+            print("{} is walking".format(self.name))
         elif moving_type == "running":
-            print ("{} is running".format(self.NAME))
+            print ("{} is running".format(self.name))
         elif moving_type == "flying":
-            print("{} is flying".format(self.NAME))
+            print("{} is flying".format(self.name))
 
 
-class Duck(Animal, Bird):
-    SWIMMING_SPEED = 0
+class Duck(Bird):
+    swimming_speed = 0
 
-    def __init__(self, name, color, speed, flying_speed, swimming_speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
-        self.FLYING_SPEED = flying_speed
-        self.SWIMMING_SPEED = swimming_speed
+    def __init__(self, swimming_speed, flying_speed, name, color, is_alive, speed):
+        super().__init__(flying_speed, name, color, is_alive, speed)
+        self.swimming_speed = swimming_speed
 
     def move(self, moving_type):
         if moving_type == "walking":
-            print("{} is walking".format(self.NAME))
+            print("{} is walking".format(self.name))
         elif moving_type == "running":
-            print ("{} is running".format(self.NAME))
+            print ("{} is running".format(self.name))
         elif moving_type == "flying":
-            print("{} is flying".format(self.NAME))
+            print("{} is flying".format(self.name))
         elif moving_type == "swimming":
-            print("{} is swimming".format(self.NAME))
+            print("{} is swimming".format(self.name))
 
     def quack(self):
         print("quack")
 
 
-class Han(Animal, Bird):
-    def __init__(self, name, color, speed, flying_speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
-        self.FLYING_SPEED = flying_speed
+class Han(Bird):
+    def __init__(self, flying_speed, name, color, is_alive, speed):
+        super().__init__(flying_speed, name, color, is_alive, speed)
 
     def crow(self):
         print("crow")
 
 
-class Goose(Animal, Bird, Duck):
-    def __init__(self, name, color, speed, flying_speed, swimming_speed):
-        self.NAME = name
-        self.COLOR = color
-        self.SPEED = speed
-        self.FLYING_SPEED = flying_speed
-        self.SWIMMING_SPEED = swimming_speed
+class Goose(Duck):
+    def __init__(self, swimming_speed, flying_speed, name, color, is_alive, speed):
+        super().__init__(swimming_speed, flying_speed, name, color, is_alive, speed)
 
     def hiss(self):
         print("shhhhh")
 
 
+burenka = Cow("burenka", "black/white", True, 2)
+guns = Goat("guns", "grey", True, 1)
+manya = Sheep("manya", "white", True, 1)
+scroodge = Duck(2, 3, "scroodge", "white", True, 1)
+ryaba = Han(1, "ryaba", "gray", True, 1)
+gusena = Goose(3, 2, "gusena", "pink", True, 1)
 
-
+gusena.hiss()
+burenka.moo()
+guns.bleat()
+manya.die()
+scroodge.quack()
+ryaba.move("running")
